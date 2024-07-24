87836182a4db20307739bbf6bc9471a1d3ece352
Juan LB <ljuan7397@gmail.com>
2024-07-24 18:45:43 +0200

    create use case

---
+++ src/modules/courses/application/get-courses.use-case.ts
@@ -0,0 +1,39 @@
+import {
+	CompletionDTO,
+	CourseResponseDTO,
+} from '../controllers/courses.controllers';
+import { Course } from '../models/Course';
+import { CoursePortRepository } from '../models/course.port.repository';
+import { CourseService } from '../models/course.service';
+import { toDto } from './toDto';
+
+export class GetCoursesUseCase {
+	constructor(
+		private readonly courseRepository: CoursePortRepository,
+		private readonly courseService: CourseService,
+	) {}
+
+	async run(): Promise<CourseResponseDTO[]> {
+		const courses = await this.courseRepository.getAll();
+
+		const courseResponses = await Promise.all(
+			courses.map(async (course: Course) => {
+				const [progress, totalLessons, completedLessons] = await Promise.all([
+					this.courseService.getCourseProgress(course.id),
+					this.courseService.getTotalLessonsCount(course.id),
+					this.courseService.getCompletedLessonsCount(course.id),
+				]);
+
+				const completionDto: CompletionDTO = {
+					total_lessons: totalLessons,
+					completed_lessons: completedLessons,
+					percentage: progress,
+				};
+
+				return toDto(course, completionDto);
+			}),
+		);
+
+		return courseResponses;
+	}
+}
