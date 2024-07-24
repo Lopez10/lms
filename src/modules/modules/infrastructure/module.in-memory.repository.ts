6fae5cce4f10c76a49a73595b3d417ef180ee97a
Juan LB <ljuan7397@gmail.com>
2024-07-24 20:03:01 +0200

    Implement in-memory repository for modules

---
+++ src/modules/modules/infrastructure/module.in-memory.repository.ts
@@ -0,0 +1,21 @@
+import { Id } from '../../../shared';
+import { Module } from '../models/Module';
+import { ModulePortRepository } from '../models/module.port.repository';
+
+export class ModuleInMemoryRepository implements ModulePortRepository {
+	private readonly modules: Module[] = [];
+
+	insert(module: Module): Promise<void> {
+		this.modules.push(module);
+
+		return Promise.resolve();
+	}
+	getById(id: Id): Promise<Module | null> {
+		const module = this.modules.find((module) => module.id.equals(id));
+
+		return Promise.resolve(module || null);
+	}
+	getAll(): Promise<Module[]> {
+		return Promise.resolve(this.modules);
+	}
+}
