f220ce2af11c92e08d24fce72bb17163d32bc03d
Juan LB <ljuan7397@gmail.com>
2024-07-24 09:03:21 +0200

    refactor module props name

--- src/modules/modules/models/Module.ts
+++ src/modules/modules/models/Module.ts
@@ -1,33 +1,25 @@
-
-export interface IModule {
-  id: string;
-  title: string;
-  isRootModule: boolean;
-  moduleId: string;
-  courseId: string;
+export interface ModuleProps {
+	id: string;
+	title: string;
+	isRootModule: boolean;
+	moduleId: string;
+	courseId: string;
 }
 
-class Module implements IModule {
+class Module implements ModuleProps {
+	id: string;
+	title: string;
+	isRootModule: boolean;
+	moduleId: string;
+	courseId: string;
 
-  id: string;
-  title: string;
-  isRootModule: boolean;
-  moduleId: string;
-  courseId: string;
-
-  constructor({
-    id,
-    title,
-    isRootModule,
-    moduleId,
-    courseId,
-  }: IModule) {
-    this.id = id;
-    this.title = title;
-    this.isRootModule = isRootModule;
-    this.moduleId = moduleId;
-    this.courseId = courseId;
-  }
+	constructor({ id, title, isRootModule, moduleId, courseId }: ModuleProps) {
+		this.id = id;
+		this.title = title;
+		this.isRootModule = isRootModule;
+		this.moduleId = moduleId;
+		this.courseId = courseId;
+	}
 }
 
 export default Module;
