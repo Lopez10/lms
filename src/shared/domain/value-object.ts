6f268ab2eca5f4243eda060eb2ba9d2e850a137a
Juan LB <ljuan7397@gmail.com>
2024-07-24 09:18:38 +0200

    create new value object class

---
+++ src/shared/domain/value-object.ts
@@ -0,0 +1,18 @@
+interface ValueObjectProps {
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	[index: string]: any;
+}
+
+export abstract class ValueObject<T extends ValueObjectProps> {
+	constructor(protected props: T) {}
+
+	public equals(vo?: ValueObject<T>): boolean {
+		if (vo === null || vo === undefined) {
+			return false;
+		}
+		if (vo.props === undefined) {
+			return false;
+		}
+		return JSON.stringify(this.props) === JSON.stringify(vo.props);
+	}
+}
