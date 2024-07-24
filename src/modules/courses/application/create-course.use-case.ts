4a2b14f462a923327526bed0559fc9eeb78e885b
Juan LB <ljuan7397@gmail.com>
2024-07-24 13:35:09 +0200

    add create course use case

---
+++ src/modules/courses/application/create-course.use-case.ts
@@ -0,0 +1,16 @@
+import { Course } from '../models/Course';
+import { CoursePortRepository } from '../models/course.port.repository';
+
+export interface CreateCourseDto {
+	title: string;
+}
+
+export class CreateCourse {
+	constructor(private readonly courseRepository: CoursePortRepository) {}
+
+	async run(courseDto: CreateCourseDto): Promise<void> {
+		const course = Course.create({ title: courseDto.title });
+
+		await this.courseRepository.insert(course);
+	}
+}
