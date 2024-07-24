c10335083c7ade7cf4c1cd7d41195de23a4d42d6
Juan LB <ljuan7397@gmail.com>
2024-07-24 19:29:48 +0200

    fix mapper

--- src/modules/modules/application/create-module.use-case.ts
+++ src/modules/modules/application/create-module.use-case.ts
@@ -1,3 +1,5 @@
+import { Id } from '../../../shared';
+import { Module } from '../models/Module';
 import { ModulePortRepository } from '../models/module.port.repository';
 import { ModuleMapper, ModuleResponseDTO } from './module.mapper';
 
@@ -12,7 +14,17 @@ export class CreateModuleUseCase {
 	constructor(private readonly moduleRepository: ModulePortRepository) {}
 
 	async run(createModuleDto: CreateModuleDto): Promise<ModuleResponseDTO> {
-		const module = ModuleMapper.toDomain(createModuleDto);
+		const moduleId = createModuleDto.moduleId
+			? Id.createExisted(createModuleDto.moduleId)
+			: undefined;
+
+		const module = Module.create({
+			title: createModuleDto.title,
+			isRootModule: createModuleDto.isRootModule,
+			moduleId,
+			courseId: Id.createExisted(createModuleDto.courseId),
+		});
+
 		await this.moduleRepository.insert(module);
 
 		return ModuleMapper.toDto(module);
