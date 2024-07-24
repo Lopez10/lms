2ec0ec0b1652ce2077396455108b21ae4b633d7d
Juan LB <ljuan7397@gmail.com>
2024-07-24 16:43:27 +0200

    implement new port

---
+++ src/modules/lessons/infrastructure/lesson.prisma.repository.ts
@@ -0,0 +1,19 @@
+import { Id } from '../../../shared';
+import { prisma } from '../../../shared';
+import { Lesson } from '../models/Lesson';
+import { LessonPortRepository } from '../models/lesson.port.repository';
+
+export class LessonPrismaRepository implements LessonPortRepository {
+	insert(lesson: Lesson): Promise<void> {
+		throw new Error('Method not implemented.');
+	}
+	getById(id: Id): Promise<Lesson | null> {
+		throw new Error('Method not implemented.');
+	}
+	getAll(): Promise<Lesson[]> {
+		throw new Error('Method not implemented.');
+	}
+	countTotalLessonByCourseId(courseId: Id): Promise<number> {
+		throw new Error('Method not implemented.');
+	}
+}
