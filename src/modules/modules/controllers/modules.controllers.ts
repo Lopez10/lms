0089f9d68e1959b8f905ce8893f69bac50a919f0
Juan LB <ljuan7397@gmail.com>
2024-07-24 09:24:24 +0200

    refactor module entity

--- src/modules/modules/controllers/modules.controllers.ts
+++ src/modules/modules/controllers/modules.controllers.ts
@@ -3,26 +3,26 @@ import { sendMethodNotImplemented } from "../../../shared/responses.utils";
 import { LessonResponseDTO } from "../../lessons/controllers/lessons.controllers";
 
 export interface ModuleResponseDTO {
-  id: string;
-  title: string;
-  is_root_module: boolean;
-  module_id: string;
-  course_id: string;
+	id: string;
+	title: string;
+	is_root_module: boolean;
+	module_id: string;
+	course_id: string;
 }
 
 export interface ModuleResponsePopulatedDTO extends ModuleResponseDTO {
-  lessons: LessonResponseDTO[];
-  modules: ModuleResponsePopulatedDTO[];
+	lessons: LessonResponseDTO[];
+	modules: ModuleResponsePopulatedDTO[];
 }
 
 export const createModule = async (req: Request, res: Response) => {
-  return sendMethodNotImplemented(res);
+	return sendMethodNotImplemented(res);
 };
 
 export const getModuleById = async (req: Request, res: Response) => {
-  return sendMethodNotImplemented(res);
+	return sendMethodNotImplemented(res);
 };
 
 export const getModules = async (req: Request, res: Response) => {
-  return sendMethodNotImplemented(res);
+	return sendMethodNotImplemented(res);
 };
--- src/modules/modules/models/Module.ts
+++ src/modules/modules/models/Module.ts
@@ -1,23 +1,15 @@
+import { Entity } from "../../../shared/domain/entity.base";
+import { Id } from "../../../shared/domain/id.value-object";
+
 interface ModuleProps {
-	id: string;
 	title: string;
 	isRootModule: boolean;
 	moduleId: string;
 	courseId: string;
 }
 
-export class Module implements ModuleProps {
-	id: string;
-	title: string;
-	isRootModule: boolean;
-	moduleId: string;
-	courseId: string;
-
-	constructor({ id, title, isRootModule, moduleId, courseId }: ModuleProps) {
-		this.id = id;
-		this.title = title;
-		this.isRootModule = isRootModule;
-		this.moduleId = moduleId;
-		this.courseId = courseId;
+export class Module extends Entity<ModuleProps> {
+	private constructor(props: ModuleProps, id?: Id) {
+		super(props, id);
 	}
 }
