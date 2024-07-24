6d51f6bca3f9edfc279fda27150046ad02ea39c0
Juan LB <ljuan7397@gmail.com>
2024-07-24 09:02:32 +0200

    remove unnecessary static types

--- src/shared/responses.utils.ts
+++ src/shared/responses.utils.ts
@@ -1,27 +1,29 @@
-import { Response } from "express";
+import { Response } from 'express';
 
-export const sendMethodNotImplemented = (res: Response, method: string = 'Method') => {
-  return res.status(501).json({
-    message: `${method} not implemented`
-  });
-}
+export const sendMethodNotImplemented = (res: Response, method = 'Method') => {
+	return res.status(501).json({
+		message: `${method} not implemented`,
+	});
+};
 
-export const sendNotFound = (res: Response, message: string = 'Not found') => {
-  return res.status(404).json({
-    message
-  });
-}
+export const sendNotFound = (res: Response, message = 'Not found') => {
+	return res.status(404).json({
+		message,
+	});
+};
 
-export const sendBadRequest = (res: Response, message: string = 'Bad request') => {
-  return res.status(400).json({
-    message
-  });
-}
+export const sendBadRequest = (res: Response, message = 'Bad request') => {
+	return res.status(400).json({
+		message,
+	});
+};
 
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 export const sendOk = (res: Response, data: any) => {
-  return res.status(200).json(data);
-}
+	return res.status(200).json(data);
+};
 
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 export const sendCreated = (res: Response, data: any) => {
-  return res.status(201).json(data);
-}
+	return res.status(201).json(data);
+};
