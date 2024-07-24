0fc4eba7a6aac8b2f0c217bf49983f15e5db277d
Juan LB <ljuan7397@gmail.com>
2024-07-24 13:45:13 +0200

    add prisma to course repo

--- src/modules/courses/infrastructure/course.prisma.repository.ts
+++ src/modules/courses/infrastructure/course.prisma.repository.ts
@@ -1,12 +1,13 @@
 import { Id } from '../../../shared';
 import { Course } from '../models/Course';
 import { CoursePortRepository } from '../models/course.port.repository';
+import { prisma } from '../../../shared';
 
 export class CoursePrismaRepository implements CoursePortRepository {
-	getById(id: Id): Promise<Course | null> {
+	async getById(id: Id): Promise<Course | null> {
 		throw new Error('Method not implemented.');
 	}
-	getAll(): Promise<Course[]> {
+	async getAll(): Promise<Course[]> {
 		throw new Error('Method not implemented.');
 	}
 	async insert(course: Course): Promise<void> {
