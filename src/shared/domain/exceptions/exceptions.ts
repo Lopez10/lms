97cae4a85d8215ba42a12c0b496a4e9c60c62e84
Juan LB <ljuan7397@gmail.com>
2024-07-24 09:19:06 +0200

    create new exceptions

---
+++ src/shared/domain/exceptions/exceptions.ts
@@ -0,0 +1,19 @@
+import { ExceptionBase } from './exception.base';
+
+export class InvalidLength extends ExceptionBase {
+	readonly code = 'INVALID_ID_LENGTH';
+	static message = 'Invalid length';
+
+	constructor(message = InvalidLength.message) {
+		super(message);
+	}
+}
+
+export class InvalidCharacter extends ExceptionBase {
+	readonly code = 'INVALID_CHARACTER';
+	static message = 'Invalid character';
+
+	constructor(message = InvalidCharacter.message) {
+		super(message);
+	}
+}
