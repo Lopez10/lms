ea5dbd1dea51a91c2d9f6d123088da820c2dcd24
Juan LB <ljuan7397@gmail.com>
2024-07-24 09:59:07 +0200

    refactor courses response DTOs

--- src/modules/courses/controllers/courses.controllers.ts
+++ src/modules/courses/controllers/courses.controllers.ts
@@ -1,29 +1,34 @@
-import { Request, Response } from "express";
-import { sendMethodNotImplemented } from "../../../shared/responses.utils";
-import { ModuleResponseDTO, ModuleResponsePopulatedDTO } from "../../modules/controllers/modules.controllers";
+import { Request, Response } from 'express';
+import { sendMethodNotImplemented } from '../../../shared/responses.utils';
+import {
+	ModuleResponseDTO,
+	ModuleResponsePopulatedDTO,
+} from '../../modules/controllers/modules.controllers';
+
+interface CompletionDTO {
+	total_lessons: number;
+	completed_lessons: number;
+	percentage: number;
+}
 
 export interface CourseResponseDTO {
-  id: string;
-  title: string;
-  completion: {
-    total_lessons: number;
-    completed_lessons: number;
-    percentage: number;
-  }
+	id: string;
+	title: string;
+	completion: CompletionDTO;
 }
 
 export interface CourseResponsePopulatedDTO extends CourseResponseDTO {
-  modules: (ModuleResponseDTO | ModuleResponsePopulatedDTO)[];
+	modules: (ModuleResponseDTO | ModuleResponsePopulatedDTO)[];
 }
 
 export const createCourse = async (req: Request, res: Response) => {
-  return sendMethodNotImplemented(res);
+	return sendMethodNotImplemented(res);
 };
 
 export const getCourseById = async (req: Request, res: Response) => {
-  return sendMethodNotImplemented(res);
+	return sendMethodNotImplemented(res);
 };
 
 export const getCourses = async (req: Request, res: Response) => {
-  return sendMethodNotImplemented(res);
+	return sendMethodNotImplemented(res);
 };
