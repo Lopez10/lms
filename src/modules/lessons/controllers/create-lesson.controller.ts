388c1ced2aeab601d3566fdc7e7e9d51d45083de
Juan LB <ljuan7397@gmail.com>
2024-07-25 20:22:28 +0200

    implement create lesson controller

--- src/modules/lessons/controllers/create-lesson.controller.ts
+++ src/modules/lessons/controllers/create-lesson.controller.ts
@@ -1,6 +1,26 @@
 import { Request, Response } from 'express';
-import { sendMethodNotImplemented } from '../../../shared';
+import { sendCreated, sendMethodNotImplemented } from '../../../shared';
+import { LessonPrimitives } from '../domain/lesson.entity';
+import { CreateLessonUseCase } from '../application/create-lesson.use-case';
+import { LESSON_DEPENDENCIES } from './lesson.dependencies';
+import { LessonMapper } from '../application/lesson.mapper';
 
 export const createLesson = async (req: Request, res: Response) => {
-	return sendMethodNotImplemented(res);
+	try {
+		const createLessonDto: LessonPrimitives = req.body;
+		const createLessonUseCase = new CreateLessonUseCase(
+			LESSON_DEPENDENCIES.lessonRepository,
+		);
+
+		const lessonCreated = await createLessonUseCase.run(createLessonDto);
+		const responseLessonDto = LessonMapper.toDto(lessonCreated);
+
+		sendCreated(res, {
+			message: 'Lesson created successfully',
+			data: responseLessonDto,
+		});
+	} catch (error) {
+		console.error(error);
+		return sendMethodNotImplemented(res);
+	}
 };
