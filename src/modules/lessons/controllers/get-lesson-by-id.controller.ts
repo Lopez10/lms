dc9692f52b7915751ca268a95ccc384cf6f7c0e0
Juan LB <ljuan7397@gmail.com>
2024-07-25 20:24:06 +0200

    implement create lesson controller

--- src/modules/lessons/controllers/get-lesson-by-id.controller.ts
+++ src/modules/lessons/controllers/get-lesson-by-id.controller.ts
@@ -1,6 +1,18 @@
 import { Request, Response } from 'express';
-import { sendMethodNotImplemented } from '../../../shared';
+import { sendOk } from '../../../shared';
+import { GetLessonByIdUseCase } from '../application/get-lesson-by-id.use-case';
+import { LESSON_DEPENDENCIES } from './lesson.dependencies';
+import { LessonMapper } from '../application/lesson.mapper';
 
 export const getLessonById = async (req: Request, res: Response) => {
-	return sendMethodNotImplemented(res);
+	const getLessonByIdUseCase = new GetLessonByIdUseCase(
+		LESSON_DEPENDENCIES.lessonRepository,
+	);
+
+	const { id } = req.params;
+
+	const lesson = await getLessonByIdUseCase.run(id);
+	const responseLessonDto = LessonMapper.toDto(lesson);
+
+	return sendOk(res, responseLessonDto);
 };
