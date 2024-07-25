49c114b3d50c9ec465ce8d9f8a41fb6e8ef8acea
Juan LB <ljuan7397@gmail.com>
2024-07-25 10:19:32 +0200

    add use case skeleton test

---
+++ src/test/modules/completions/create-completion.use-case.test.ts
@@ -0,0 +1,11 @@
+describe('create completion use case', () => {
+	it(`
+		GIVEN there is a lesson id and a user id
+		WHEN I create a completion
+		THEN the completion is created successfully
+	`, () => {
+		// GIVEN
+		// WHEN
+		// THEN
+	});
+});
