d77b247efc763ea876782abb91b3a8bcc363e444
Juan LB <ljuan7397@gmail.com>
2024-07-25 20:04:12 +0200

    create new controller

--- src/modules/completions/controllers/create-completion.controller.ts
+++ src/modules/completions/controllers/create-completion.controller.ts
@@ -1,6 +1,27 @@
 import { Request, Response } from 'express';
-import { sendMethodNotImplemented } from '../../../shared/responses.utils';
+import { CompletionPrimitives } from '../domain/completion.entity';
+import { COMPLETION_DEPENDENCIES } from './completion.dependencies';
+import { CreateCompletationUseCase } from '../application/create-completation.use-case';
+import { CompletionMapper } from '../application/completion.mapper';
+import { sendBadRequest, sendCreated } from '../../../shared';
 
 export const createCompletion = async (req: Request, res: Response) => {
-	return sendMethodNotImplemented(res);
+	try {
+		const createCompletionDto: CompletionPrimitives = req.body;
+		const createCompletionUseCase = new CreateCompletationUseCase(
+			COMPLETION_DEPENDENCIES.completionPrismaRepository,
+		);
+
+		const completionCreated =
+			await createCompletionUseCase.run(createCompletionDto);
+		const responseCompletionDto = CompletionMapper.toDto(completionCreated);
+
+		sendCreated(res, {
+			message: 'Completion created successfully',
+			data: responseCompletionDto,
+		});
+	} catch (error) {
+		console.error(error);
+		return sendBadRequest(res);
+	}
 };
