c6aae238fd559fbf6c253d54a497d39460f7c333
Juan LB <ljuan7397@gmail.com>
2024-07-26 10:35:55 +0200

    refactor create lesson

--- src/modules/lessons/controllers/create-lesson.controller.ts
+++ src/modules/lessons/controllers/create-lesson.controller.ts
@@ -5,6 +5,8 @@ import { CreateLessonUseCase } from '../application/create-lesson.use-case';
 import { LESSON_DEPENDENCIES } from './lesson.dependencies';
 import { LessonMapper } from '../application/lesson.mapper';
 
+const EMPTY_COMPLETED = false;
+
 export const createLesson = async (req: Request, res: Response) => {
 	try {
 		const createLessonDto: LessonPrimitives = req.body;
@@ -13,7 +15,10 @@ export const createLesson = async (req: Request, res: Response) => {
 		);
 
 		const lessonCreated = await createLessonUseCase.run(createLessonDto);
-		const responseLessonDto = LessonMapper.toDto(lessonCreated);
+		const responseLessonDto = LessonMapper.toDtoWithCompletion(
+			lessonCreated,
+			EMPTY_COMPLETED,
+		);
 
 		sendCreated(res, {
 			message: 'Lesson created successfully',
