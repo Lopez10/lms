e3946814da8e63e17fbe1c323e5b1109659eaada
Juan LB <ljuan7397@gmail.com>
2024-07-26 12:22:58 +0200

    create use case

---
+++ src/test/modules/users/create-user.use-case.test.ts
@@ -0,0 +1,26 @@
+import { CreateUserUseCase } from '../../../modules/users/application/create-user.use-case';
+import { UserInMemoryRepository } from '../../../modules/users/infrastructure/user.in-memory.repository';
+
+describe('create user use case', () => {
+	it(`
+        GIVEN there are a name and email
+        WHEN I create a user
+        THEN the user is created successfully
+    `, async () => {
+		const userRepository = new UserInMemoryRepository();
+		const createUser = new CreateUserUseCase(userRepository);
+
+		// GIVEN
+		const name = 'user name';
+		const email = 'email@example.com';
+
+		// WHEN
+		const user = await createUser.run({ name, email });
+
+		// THEN
+		expect(user).toBeDefined();
+		expect(user.id).toBeDefined();
+		expect(user.props.name).toBe(name);
+		expect(user.props.email.value).toBe(email);
+	});
+});
