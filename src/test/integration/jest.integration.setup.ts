8353af7592e73dce7e21913e0c399ab0251bf2b3
Juan LB <ljuan7397@gmail.com>
2024-07-29 17:32:10 +0200

    clean up integration tests and update dependencies

--- src/test/integration/jest.integration.setup.ts
+++ src/test/integration/jest.integration.setup.ts
@@ -2,12 +2,20 @@ import { server } from '../..';
 import { prisma } from '../../shared';
 
 afterAll(async () => {
+	await cleanDatabase();
+
+	await prisma.$disconnect();
+	server.close();
+});
+
+beforeAll(async () => {
+	await cleanDatabase();
+});
+
+async function cleanDatabase() {
 	await prisma.lesson.deleteMany();
 	await prisma.module.deleteMany();
 	await prisma.course.deleteMany();
 	await prisma.user.deleteMany();
 	await prisma.completion.deleteMany();
-
-	await prisma.$disconnect();
-	server.close();
-});
+}
